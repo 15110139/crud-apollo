a2963ea559764004b69dac7f8fc5e5d8e0b6e059
vang <luongquocvang@gmail.com>
2019-02-27 11:44:07 +0700

    tien

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React, { Component } from 'react';
-import { graphql } from 'react-apollo';
-import { getListUser } from './apollo/query';
-
+import React, { Component } from "react";
+import {Row, Col} from 'antd'
+import TabConten from "./Todo/component/TabConten";
+import MenuNav from './Todo/component/MenuNav'
 class App extends Component {
-	render() {
-		console.log(this.props);
-		return <div>hihi</div>;
-	}
+  render() {
+    // console.log('prop',this.props);
+
+    return (
+      <div>
+        <Row>
+          <Col span={5}>
+            <MenuNav />
+          </Col>
+          <Col span={19}>
+            <TabConten />
+          </Col>
+        </Row>
+      </div>
+    );
+  }
 }
 
-export default graphql(getListUser)(App);
+export default App;
